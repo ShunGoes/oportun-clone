c958728e5aa6df5c54967f0b4b08affdacb654f5
wyenelle <favourkcb@gmail.com>
2023-12-15 08:52:17 +0100

    adding the ios and google download buttons

--- src/component/nav-dropdown/dropdown.jsx
+++ src/component/nav-dropdown/dropdown.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 import { CiWarning } from "react-icons/ci";
 
+import GoogleLogo from '../../assets/google.png'
+import IosLogo from '../../assets/ios.png'
+
 const Dropown = ({ currentTab }) => {
   const data = {
     loans: {
@@ -27,7 +30,7 @@ const Dropown = ({ currentTab }) => {
         "Get the all-in-one app to manage your loan. Plus it intelligently saves and invests for you.",
     },
     save: {
-        name: 'save',
+      name: "save",
       main_arr: ["Set & Save", "Help", "Contact"],
       tools: [
         "FAQs",
@@ -42,7 +45,7 @@ const Dropown = ({ currentTab }) => {
         "Get the all-in-one app that intelligently saves and invests for you.",
     },
     creditcards: {
-        name: 'credit card',
+      name: "Credit Card",
       main_arr: ["Credit Card", "Make a payment", "Contact Us", "Location"],
       tools: [
         "FAQs",
@@ -56,8 +59,8 @@ const Dropown = ({ currentTab }) => {
       addons:
         "Get the all-in-one app to manage your loan. Plus it intelligently saves and invests for you.",
     },
-    loan: {
-      loans: [
+    login: {
+      main_arr: [
         "Personnal Loans",
         "Secured Personnal Loan",
         "Make a payment",
@@ -78,54 +81,84 @@ const Dropown = ({ currentTab }) => {
         "Get the all-in-one app to manage your loan. Plus it intelligently saves and invests for you.",
     },
   };
-  const array_of_keys = Object.keys(data)
+  const array_of_keys = Object.keys(data);
   const position_of_elem = array_of_keys.indexOf(currentTab);
-  const final_object = data[array_of_keys[position_of_elem]]
+  const final_object = data[array_of_keys[position_of_elem]];
   console.log(final_object);
   return (
-    <section className="h-[23rem] bg-black text-white  w-full border-red-500 top top-[148px]  absolute hidden md:block">
-      <div className="w-[80%] h-4/5  mx-auto border-red-600 grid grid-cols-4 ">
-        <div className=" h-4/6 mt-5 ">
-          <strong className="links"> {final_object.name} </strong>
-          <div className="border-4 h-4/5 border-yellow-500 mt-1 flex flex-col">
-            {final_object.main_arr.map((item) => (
-                <a key={item} className="font-[18px]">
-                {item}
-                </a>
-            ))}
-          </div>
-        </div>
-        <div className="grid grid-rows-2">
-          <div className="border-4 h-4/8 border-green-500 flex flex-col ">
-            <strong className="links"> Tools </strong>
-            <a className="font-[18px] ">
-              {final_object.tools.map((item) => (
+    <section className="h-[23rem] bg-black text-white  w-full  top top-[148px]  absolute hidden md:block">
+      {final_object.name ? (
+        <div className="w-[79%] h-4/5  mx-auto  grid grid-cols-4 ">
+          <div className=" h-4/6 mt-5 ">
+            <strong className="links"> {final_object.name} </strong>
+            <div className=" h-4/5  mt-1 flex flex-col">
+              {final_object.main_arr.map((item) => (
                 <a key={item} className="font-[18px]">
                   {item}
                 </a>
               ))}
-            </a>
+            </div>
           </div>
-          <div className="border-4 h-4/8 border-green-500 flex flex-col ">
-            <strong className="links"> Get Savvy </strong>
-            <div className="border-4 h-4/5 border-yellow-500 mt-1 flex flex-col">
-                {final_object.get_savy.map((item) => (
-                <a key={item} className="font-[18px]">
+          <div className="grid grid-rows-2">
+            <div className=" h-7/12  mt-4 ">
+              <strong className="links"> Tools </strong>
+              <div className=" h-4/5 mt-1   flex flex-col">
+                {final_object.tools.map((item) => (
+                  <a key={item} className="font-[18px]">
                     {item}
-                </a>
+                  </a>
                 ))}
-
+              </div>
+            </div>
+            <div className=" h-4/8   mt-5 ">
+              <strong className="links"> Get Savvy </strong>
+              <div className=" h-4/5  mt-1 flex flex-col">
+                {final_object.get_savy.map((item) => (
+                  <a key={item} className="font-[18px]">
+                    {item}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="col-span-2  mt-5">
+            <div className=" flex flex-col gap-5 w-8/12 mx-auto  ">
+              <div className="flex justify-center items-center h-28 w-24 bg-slate-300">
+                <CiWarning size={20} color="red" />
+              </div>
+              <p className="6/12">{final_object.addons}</p>
+              <div className="w-10/12  h-24 border grid grid-cols-2">
+                <img src={IosLogo} alt='download on the app store' className="h-full"/>
+                <img src={GoogleLogo} alt='download on the google play store' className="h-full"/>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-span-2 flex flex-col gap-5">
-          <div className="flex justify-center items-center h-28 w-24 bg-slate-300">
-            <CiWarning size={20} color="red" />
+      ) : (
+        <div className="w-[79%] h-11/12  mx-auto  grid grid-cols-4">
+          <div className="col-span-2 mt-5 h-full">
+            <div className=" flex flex-col gap-5 w-8/12  ">
+              <div className="flex justify-center items-center h-28 w-24 bg-slate-300">
+                <CiWarning size={20} color="red" />
+              </div>
+              <p className="6/12">{final_object.addons}</p>
+              <div className="w-10/12  h-24 border">
+              
+              </div>
+            </div>
           </div>
-          <p className="6/12">{final_object.addons}</p>
-          <div className="w-6/12  h-24 border-4"></div>
+          <div className="col-span-2 mt-2 flex justify-end">
+          <div className="  w-[62%] h-[90%] mt-5">
+            <strong className="links">Select an account to log in </strong>
+            <div className="flex flex-col mt-2">
+                <a className="font-[18px]"> Loans or Credit Card</a>
+                <a className="font-[18px]"> Saving</a>
+                <a className="font-[18px]"> Sezzle credit accounts</a>
+            </div>
+          </div>
+          </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };
