09abcc4d3fdc136801ce7eb048002ca089e95039
wyenelle <favourkcb@gmail.com>
2023-12-16 23:02:19 +0100

    chsnging shadows

--- src/component/calculator/calculator.jsx
+++ src/component/calculator/calculator.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Calculator = () => {
   return (
     <section className='p-5 h-auto'>
-        <div className="border-2 border-slate-400/60 h-full rounded-lg">
+        <div className="shadow-lg border-2 shadow-gray-400/60 h-full rounded-lg">
             <div className='p-5 h-[45%]  flex flex-col gap-4'>
                 <h3 className='text-[34px] font-[600]  leading-[37px]'> Loan Calculator</h3>
                 <span className="text-gray-400 italic text-[14px]">What could your loan payments look like</span>
-                <input type="text" placeholder='Where do you live ?' className='border border-slate-50 p-3 h-[50px] w-full bg-transparent rounded-lg ' />
+                <input type="text" placeholder='Where do you live ?' className='border border-gray-50 p-3 h-[50px] w-full bg-transparent rounded-lg ' />
             </div>
 
             <div>
-            <div className="border-b-2 border-b-50 py-5 border-slate-50 w-11/12 mx-auto rounded-xl">
+            <div className="shadow-lg border-b-50 py-5 shadow-slate-500/50 w-11/12 mx-auto rounded-2xl">
                 <span className="text-gray-500  text-[14px]"> Loan Amount</span>
                 <h1 className='text-[34px] font-[800]  leading-[37px] mt-2'>$10,000 </h1>
             </div>
